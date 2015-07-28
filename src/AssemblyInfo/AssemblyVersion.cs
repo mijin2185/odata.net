d42ba964445781da04ea61b7323da253c94834fe
Mike Jin <mijin@microsoft.com>
2015-07-28 16:24:50 -0700

    Fix assembly version

--- src/AssemblyInfo/AssemblyVersion.cs
+++ src/AssemblyInfo/AssemblyVersion.cs
@@ -12,13 +12,13 @@ using System.Resources;
 // The following assembly information is common to all product assemblies.
 // If you get compiler errors CS0579, "Duplicate '<attributename>' attribute", check your 
 // Properties\AssemblyInfo.cs file and remove any lines duplicating the ones below.
-[assembly: AssemblyFileVersion("6.11.0.62179")]
-[assembly: AssemblyInformationalVersion("6.11.0.62179")]
+[assembly: AssemblyFileVersion("6.11.0.0")]
+[assembly: AssemblyInformationalVersion("6.11.0.0")]
 
 #if ASSEMBLY_ATTRIBUTE_NO_BUILD_NUM_IN_VERSION
-[assembly: AssemblyVersion("6.11.0.62179")]
-[assembly: SatelliteContractVersion("6.11.0.62179")]
+[assembly: AssemblyVersion("6.11.0.0")]
+[assembly: SatelliteContractVersion("6.11.0.0")]
 #else
-[assembly: AssemblyVersion("6.11.0.62179")]
-[assembly: SatelliteContractVersion("6.11.0.62179")]
+[assembly: AssemblyVersion("6.11.0")]
+[assembly: SatelliteContractVersion("6.11.0")]
 #endif
